85d5a1a32caab40c3ebd069349b52caf67a6a547
Soheil Nikroo <soheiln1234@gmail.com>
2023-11-22 15:56:46 +0330

    Initial Commit

---
+++ i am RIch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am RIch
+//
+//  Created by Soheil Nikroo on 11/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
